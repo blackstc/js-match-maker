9b07c08d8bf1f28d8836c9d2872942d5ac853210
Blackstock <charlieblackstock@gmail.com>
2015-07-19 12:00:18 -0600

    added JS for mentors

--- main.js
+++ main.js
@@ -16,8 +16,30 @@ for (var i = 0; i < numStudents; i++) {
   studentArr.push(contactInfo);
 }
 
+var numMentors = prompt("What is the total number of mentors, for using javascript?");
+
+var mentorArr = [];
+
+for (var i = 0; i < numMentors; i++) {
+  var contactInfo = {name: "", phoneNumber: "", city: ""};
+  var mentorName = prompt("What is your full name?");
+  var mentorPhone = prompt("What is your phone number?", "XXX-XXX-XXXX");
+  var mentorCity = prompt("What city are you from?");
+
+  contactInfo.name = mentorName;
+  contactInfo.phoneNumber = mentorPhone;
+  contactInfo.city = mentorCity;
+  mentorArr.push(contactInfo);
+}
+
 alert("The total number of students learning javascript are " + numStudents);
 
 for (var i = 0; i < numStudents; i++) {
   alert("Name: " + studentArr[i].name + "\nPhone Number: " + studentArr[i].phoneNumber + "\nCity: " + studentArr[i].city);
+};
+
+alert("The total number of mentors with javascript are " + numMentors);
+
+for (var i = 0; i < numMentors; i++) {
+  alert("Name: " + mentorArr[i].name + "\nPhone Number: " + mentorArr[i].phoneNumber + "\nCity: " + mentorArr[i].city);
 };
